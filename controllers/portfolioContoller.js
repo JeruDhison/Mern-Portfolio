f33c41320bc58a5473b202575edeaae124c9bb1b
JeruDhison <127184145+JeruDhison@users.noreply.github.com>
2024-06-07 13:52:16 +0530

    Mern Stack Portfolio
    Professional Portfolio

---
+++ controllers/portfolioContoller.js
@@ -0,0 +1,59 @@
+const nodemailer = require("nodemailer");
+const Mailgen = require("mailgen");
+let config = {
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+};
+
+let transporter = nodemailer.createTransport(config);
+
+let MailGenerator = new Mailgen({
+  theme: "default",
+  product: {
+    name: "Mailgen",
+    link: "https://mailgen.js/",
+  },
+});
+
+const sendEmailController = (req, res) => {
+  try {
+    const { name, email, msg } = req.body;
+
+    //validation
+    if (!name || !email || !msg) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide All Fields",
+      });
+    }
+
+    transporter.sendMail({
+      to: "jerushadhison@gmail.com",
+      from: process.env.EMAIL,
+      subject: "Regarding Mern Portfolio App",
+      html: `
+        <h5>Detail Information</h5>
+        <ul>
+          <li><p>Name : ${name}</p></li>
+          <li><p>Email : ${email}</p></li>
+          <li><p>Message : ${msg}</p></li>
+        </ul>
+      `,
+    });
+    return res.status(200).send({
+      success: true,
+      message: "Your Message Send Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Send Email API Error",
+      error,
+    });
+  }
+};
+module.exports = { sendEmailController };
